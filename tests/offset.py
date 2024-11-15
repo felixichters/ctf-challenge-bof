afc4e7aadc20450858992d5bd0152b7c7807e317
felix <felix.ichters@gmail.com>
2024-11-15 21:57:50 +0100

    hm

--- tests/offset.py
+++
@@ -1,7 +0,0 @@
-from pwn import *
-
-crash_address = 0x00007ffff7a99a9c 
-offset = cyclic_find(crash_address)
-print(f"Offset is {offset}")
-
-#./a.out $(python3 -c 'from pwn import *; print(cyclic(200))')
