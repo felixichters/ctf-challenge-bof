9da257ebde5d2c4f9c09f00b3652e23d72351a4a
felix <felix.ichters@gmail.com>
2025-01-20 16:45:30 +0100

    cleanup

--- shellcode.c
+++
@@ -1,9 +0,0 @@
-#include <unistd.h>
-
-int main() {
-    char *shell = "/bin/zsh";
-    char *args[] = {shell, NULL};
-    execve(shell, args, NULL);
-    return 0;
-}
-
--- susi2.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <string.h>
-
-int main (int argc, char** argv)
-{
-	char buffer[512];
-	strcpy(buffer, argv[1]);
-	return 0;
-}
