3f31aa2fff4871ccbda6fb156f6df25a9f3f5ff6
felix <felix.ichters@gmail.com>
2025-01-28 16:02:46 +0100

    working bof exploit

--- container-a/susi.c
+++ container-a/susi.c
@@ -1,21 +1,8 @@
 #include <stdio.h>
-#include <string.h>
 
 int main() {
-    char buffer[16];
-
-    printf("Leaked address: %p\n", (void*)buffer);
-
-    printf("Enter your input: ");
-    fflush(stdout); 
-
-    char input[512];
-    fgets(input, sizeof(input), stdin);
-
-    strcpy(buffer, input);
-
-    printf("You entered: %s\n", buffer);
-    
-	return 0;
+    char buffer[256];
+    printf("Leaked: %p\n", buffer);
+		gets(buffer);
 }
 
