f659f98d3399237941c659f79b6d3db65b287367
felix <>
2025-03-15 03:21:24 +0100

    dockerette

---
+++ server/vuln.c
@@ -0,0 +1,27 @@
+
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+#define DELAY 500000
+
+int main(int argc, char * argv[]) { 
+	char * fileName = argv[1];
+	char buffer[200];
+	int i;
+	FILE * fileHandler;
+
+	scanf("%50s", buffer );
+
+	if(!access(fileName, W_OK)) {
+		for(i = 0; i < DELAY;i++) {
+			int a = i ^ 2;
+		}
+
+		fileHandler = fopen(fileName, "a+"); 
+		fwrite(buffer, sizeof(char), strlen(buffer), fileHandler); 
+		fclose(fileHandler);
+	} else {
+		printf("No permission");
+	}
+}
